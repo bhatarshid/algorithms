9d83669b32171fbe4a5332313795c8f20b6addbc
Your Name <foo@example.com>
2020-04-25 17:25:49 +0530

    The program gives the maximum profit in stock market

--- stockmarketmaximise.py
+++ stockmarketmaximise.py
@@ -9,12 +9,6 @@ def chkDecreasing(A):
             return False
     return True
 
-def chkIncreasing(A):
-    for i in range( len(A) - 1 ):
-        if A[i] > A[i+1]:
-            return False
-    return True
-
 def makelist(A,l,r):
     for i in range(A.index(max(A))+1):
         l.append(A[i])
@@ -22,38 +16,33 @@ def makelist(A,l,r):
         r.append(A[i])
     return l,r
 
-def stockmax(prices,profit,l):
+def stockmax(prices):
+    profit = 0
     if(len(prices) == 1):
         return 0
     if(chkDecreasing(prices) == True):
         profit = 0
-        prices.append(profit)
-        l.append(prices)
+    
     elif(prices[len(prices)-1] == max(prices)):
         profit = (len(prices)*prices[len(prices)-1]) - sum(prices[:len(prices)])
-        prices.append(profit)
-        l.append(prices)
+        
     else:
         llist = []
         rlist = []
         llist, rlist = makelist(prices,llist,rlist)    
-        if(llist in l):
-            profit = profit + l[l.index(llist)][len(llist)-1]
-        else:
-            profit = profit + stockmax(llist,profit,l)
-        if(rlist in l):
-            profit = profit + l[l.index(rlist)][len(rlist)-1]
-        else:
-            profit = profit + stockmax(rlist,profit,l)
+        profit = profit + stockmax(llist)
+        
+        profit = profit + stockmax(rlist)
+        
     return profit
 
 if __name__ == '__main__':
     n = int(input("Size of prices: ").strip())
     print("Enter the prices")
     prices = list(map(int, input().rstrip().split()))
-    dlist = []
+    memo = []
     prf = 0
-    result = stockmax(prices,prf,dlist)
+    result = stockmax(prices)
     print("result = ",result)
 
     
